c8f87920b585db6a4e96f607f59f9492203344f8
Jérôme Blanchard <jblanchard@jahia.com>
2025-03-18 15:07:15 +0100

    Ensure definitions converge with new systemId and remove advanced-visibility if installed

--- src/main/resources/META-INF/patches/groovy/8.2.0-01-removeDefinitions.resolved.groovy
+++ src/main/resources/META-INF/patches/groovy/8.2.0-01-removeDefinitions.resolved.groovy
@@ -1,4 +1,6 @@
 import org.jahia.services.content.JCRStoreService
+import org.jahia.osgi.FrameworkService
+import org.osgi.framework.Bundle
 
 def source = "advanced-visibility";
 
@@ -6,3 +8,13 @@ log.info("Undeploy definitions of " + source);
 JCRStoreService jcrStoreService = JCRStoreService.getInstance();
 jcrStoreService.undeployDefinitions(source);
 log.info("Successfully removed defintions for systemId: " + source);
+
+log.info("Checking if " + source + " bundle is installed");
+Bundle[] bundles = FrameworkService.getBundleContext().getBundles();
+for (Bundle bundle: bundles) {
+    if (bundle.getSymbolicName().equals(source)) {
+        log.info("Uninstalling bundle " + source);
+        bundle.uninstall();
+        log.info("Successfully uninstalled bundle " + source);
+    }
+}
