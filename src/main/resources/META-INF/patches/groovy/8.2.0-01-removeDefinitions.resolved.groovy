cd7843574cbe71d799074fe20934ccc75a8950b2
Jérôme Blanchard <jblanchard@jahia.com>
2025-03-19 22:02:11 +0100

    Change migration order to avoid errors in log.

--- src/main/resources/META-INF/patches/groovy/8.2.0-01-removeDefinitions.resolved.groovy
+++ src/main/resources/META-INF/patches/groovy/8.2.0-01-removeDefinitions.resolved.groovy
@@ -4,17 +4,17 @@ import org.osgi.framework.Bundle
 
 def source = "advanced-visibility";
 
-log.info("Undeploy definitions of " + source);
-JCRStoreService jcrStoreService = JCRStoreService.getInstance();
-jcrStoreService.undeployDefinitions(source);
-log.info("Successfully removed defintions for systemId: " + source);
-
-log.info("Checking if " + source + " bundle is installed");
+log.info("Checking if bundle with symbolic name " + source + " needs to be uninstalled");
 Bundle[] bundles = FrameworkService.getBundleContext().getBundles();
 for (Bundle bundle: bundles) {
     if (bundle.getSymbolicName().equals(source)) {
-        log.info("Uninstalling bundle " + source);
+        log.info("Bundle " + source + " is present in version " + bundle.getVersion().toString() + ", uninstalling... ");
         bundle.uninstall();
         log.info("Successfully uninstalled bundle " + source);
     }
 }
+
+log.info("Undeploy definitions of " + source);
+JCRStoreService jcrStoreService = JCRStoreService.getInstance();
+jcrStoreService.undeployDefinitions(source);
+log.info("Successfully removed defintions for systemId: " + source);
